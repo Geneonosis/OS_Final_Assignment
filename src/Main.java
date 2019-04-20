0b99301313fc4853592fd7b4894e6137a47e75fa
Gene Tigner <genetigner.art@gmail.com>
2019-04-20 16:04:03 -0500

    created an input file and started the main class that will obtain a file and read its contents

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,49 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
-
+	
+	static File file = null;
+	static BufferedReader input = null;
+	
 	public static void main(String[] args) {
+		getFile("./src/" + args[0]);
+		readAndPrintFile();
+	}
+
+	/***
+	 * Funciton: readAndPrintFile()
+	 * Purpose: to read a file and print its contents into the console
+	 * TODO: Break up later?
+	 */
+	public static void readAndPrintFile() {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		try {
+			input = new BufferedReader(new FileReader(file));
+			String str = "";
+			while((str = input.readLine()) != null) {
+				System.out.println(str);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/***
+	 * Function: getFile
+	 * Purpose: to get a file to cache
+	 * @param fileName
+	 */
+	public static void getFile(String fileName) {
+		// TODO Auto-generated method stub
+		file = new File(fileName);
 	}
 
 }
